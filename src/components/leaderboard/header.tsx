07aa095727ee94c15161971b235fb096f8f26974
Dorian <dorian@latouche.dev>
2022-12-19 16:00:29 +0100

    improve reponsive

--- src/components/leaderboard/header.tsx
+++ src/components/leaderboard/header.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Header = () => {
   return (
-    <div className="bg-primary-200 grid grid-cols-leaderboard-row shadow-container items-center h-[5em] w-[35em]">
+    <div className="bg-primary-200 grid grid-cols-leaderboard-row shadow-container items-center h-[5em] w-[35em] xs:w-[25em]">
       <p className="text-text-main ml-2">Rank</p>
       <p className="text-text-main ml-2">Name</p>
       <p className="text-text-main ml-2 ">Score</p>
--- src/features/home/home.tsx
+++ src/features/home/home.tsx
@@ -10,11 +10,11 @@ export const Home = () => {
   const classNameHome = `w-full h-full flex flex-col justify-center items-center min-h-screen pl-[0.125em] pt-[0.125em] overflow-auto`;
   return (
     <div className={classNameHome}>
-      <div className="bg-black h-[5em] w-[15em] mt-[5em] flex justify-center items-center shadow-container">
+      <div className="bg-black h-[5em] w-[15em] mt-[5em]  xs:mt-[0em] flex justify-center items-center shadow-container">
         <img className="" src={logoLetter} alt="logo" />
       </div>
 
-      <div className="relative h-[15em] w-[65em] lg:w-[55em] md:w-[45em] 1.5xs:w-[35em] xs:w-[25em] mt-[5em] bg-black flex justify-center items-center shadow-container">
+      <div className="relative  h-[15em] xs:h-[10em] w-[65em] lg:w-[55em] md:w-[45em] 1.5xs:w-[35em] xs:w-[25em] mt-[5em] xs:mt-[5em] bg-black flex justify-center items-center shadow-container">
         {half() && <Star color={randomArray(COLORS)} size={2} left={-1} />}
         {half() && <Star color={randomArray(COLORS)} size={2} right={-1} />}
         {half() && <Star color={randomArray(COLORS)} size={2} top={-1} />}
